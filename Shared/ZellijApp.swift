60a8015611038ea52e1acd9b7879569741ee5bae
Sami Suteria <sami.suteria@gmail.com>
2020-09-07 13:52:02 -0500

    Initial Commit

---
+++ Shared/ZellijApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZellijApp.swift
+//  Shared
+//
+//  Created by Sami Suteria on 9/7/20.
+//
+
+import SwiftUI
+
+@main
+struct ZellijApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
